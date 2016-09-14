94fd77147af21bb9fe6c6bc52da0e30d539b3603
Arthur Christoph <achristoph@gmail.com>
2016-09-14 00:51:52 -0500

    .

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Subject, BehaviorSubject, ReplaySubject } from 'rxjs/Rx';
 @Component({
   directives: [],
   selector: 'dashboard',
@@ -17,4 +17,28 @@ export class DashboardComponent implements OnInit {
     //
   }
 
+  
 }
+
+var subject = new ReplaySubject(1 /* buffer size */);
+
+subject.next('a');
+subject.next('b');
+subject.next('c');
+
+var subscription = subject.subscribe(
+    function (x) {
+        console.log('Next: ' + x.toString());
+    },
+    function (err) {
+        console.log('Error: ' + err);
+    },
+    function () {
+        console.log('Completed');
+    });
+
+// => Next: b
+// => Next: c
+
+subject.next('d');
+// => Next: d
