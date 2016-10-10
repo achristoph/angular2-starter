406f9c41c7b4936d4d5a41fd09c1a2167b35a9e4
achristoph <achristoph@gmail.com>
2016-10-10 01:37:32 -0500

    .

--- src/app/core/services/wikipedia.service.ts
+++ src/app/core/services/wikipedia.service.ts
@@ -16,9 +16,10 @@ export class WikipediaService {
   }
 
   search(terms: Observable<string>, debounceMs = 400): Observable<string> {
-    return terms.debounceTime(400)
-      .distinctUntilChanged()
-      .switchMap(term => this.searchRaw(term));
+    return terms.debounceTime(400) // gives enough pause for the request between keystroke
+      .distinctUntilChanged() // deleting a character and redo the same character will not trigger another request
+      .switchMap(term => this.searchRaw(term)); // switchMap handles out of order reponses
+    // - when there's a delay of previous autocomplete, that autocomplete is ignored
   }
 
 }
